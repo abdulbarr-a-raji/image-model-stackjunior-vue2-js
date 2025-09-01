0f1f87dd1207bf3d60313c42afa8cc689934a559
abdulbarraraji <abdulbarr.a.raji@gmail.com>
2025-09-01 15:05:23 +0400

    changes made to train and save a default model for Stackjunior AI BIrdiad image classification model sharing page

--- src/components/Trainer.vue
+++ src/components/Trainer.vue
@@ -6,6 +6,10 @@
     <br>
     <input type="file" id="imagedataUpload" multiple accept="image/*" />
     <button id="load-images" v-on:click="runMain">Load Image Data & Train Model</button>
+    <hr>
+    <button id="train-with-extractor" v-on:click="fitClassifcationHead">
+      Load Image Data & Train Classification Head
+    </button>
   </div>
 </template>
 
@@ -22,7 +26,12 @@ export default {
   },
   data() {
     return {
-      tf: null
+      tf: null,
+      featureExtractor: null,
+      fc_head: null,
+
+      // FLAGS
+      trainingWithFeatureExtractor: false
     }
   },
   methods: {
@@ -76,7 +85,7 @@ export default {
         reader.readAsDataURL(file);
       });
     },
-    checkForTFJS: function () {
+    checkFortf: function () {
       if (this.tf) {
         console.log("tf.js is in this scope :D");
       } else {
@@ -84,12 +93,52 @@ export default {
       }
     },
     preprocessImage: function (img) {
-      return this.tf.tidy(() => {
-        return this.tf.browser.fromPixels(img)
-        .resizeBilinear([this.w, this.h])
-        .toFloat()
-        .div(this.tf.scalar(255));
-      });
+      switch(this.trainingWithFeatureExtractor) {
+        case true:
+          return this.tf.tidy(() => {
+            const tns = this.tf.browser.fromPixels(img);
+            
+            // setting crop size operation...
+            const widthToHeight = tns.shape[1] / tns.shape[0];
+            let croppedSize;
+
+            if (widthToHeight > 1) {
+              // Image is wider than tall? crop sides
+              const heightToWidth = tns.shape[0] / tns.shape[1];
+              const cropTop = (1 - heightToWidth) / 2;
+              const cropBottom = 1 - cropTop;
+              croppedSize = [[cropTop, 0, cropBottom, 1]];
+            } else {
+              // Image is taller than wide? crop top & bottom
+              const cropLeft = (1 - widthToHeight) / 2;
+              const cropRight = 1 - cropLeft;
+              croppedSize = [[0, cropLeft, 1, cropRight]];
+            }
+
+            // crop, resize, and more...
+            const croppedImgTns = this.tf.image.cropAndResize(
+              tns.expandDims(0),
+              croppedSize,
+              [0],
+              [224, 224]
+            ).toFloat()
+              .div(255.0);
+            
+            // disposal/s
+            tns.dispose();
+
+            return this.featureExtractor.predict(croppedImgTns).squeeze();
+          });
+        case false:
+          return this.tf.tidy(() => {
+            return this.tf.browser.fromPixels(img)
+            .resizeBilinear([this.w, this.h])
+            .toFloat()
+            .div(this.tf.scalar(255));
+          });
+        default:
+          return null;
+      }
     },
     getCNNModel: function () {            // getCNNModel function
       const model = this.tf.sequential();
@@ -135,6 +184,98 @@ export default {
       return model;
 
     },
+    async fitClassifcationHead() {
+      this.trainingWithFeatureExtractor = true;
+      const NUM_CLASSES = ['sad', 'smiling'].length;
+
+      // load feature extractor
+      try {
+        const extractorURL =
+          "https://storage.googleapis.com/jmstore/TensorFlowJS/EdX/SavedModels/mobilenet-v2/model.json";
+        const loadedModel = await this.tf.loadLayersModel(extractorURL);
+
+        // Create feature extraction model
+        const beforeFinalLayer = loadedModel.getLayer(
+          "global_average_pooling2d_1"
+        );
+        this.featureExtractor = this.tf.model({
+          inputs: loadedModel.inputs,
+          outputs: beforeFinalLayer.output,
+        });
+
+        // Warm up the model
+        this.tf.tidy(() => {
+          const warmupInput = this.tf.zeros([1, 224, 224, 3]);
+          const answer = this.featureExtractor.predict(warmupInput);
+          answer.print();
+        });
+
+        console.log("MobileNet v2 feature extractor created successfully");
+      } catch (error) {
+        console.error(`Error setting up the feature extractor: ${error}`);
+        throw error;
+      }
+
+      // define architecture of classification head
+      this.fc_head = this.tf.sequential();
+      this.fc_head.add(
+        this.tf.layers.dense({
+          inputShape: [this.featureExtractor.outputs[0].shape[1]],
+          units: 64,
+          activation: "relu",
+        })
+      );
+      this.fc_head.add(
+        this.tf.layers.dense({
+          units: NUM_CLASSES,
+          activation: "softmax",
+        })
+      );
+
+      // Compile classification head
+      const model_optimizer = this.tf.train.adam(0.01);
+      this.fc_head.compile({
+        optimizer: model_optimizer,
+        loss:
+          NUM_CLASSES === 2
+            ? "binaryCrossentropy"
+            : "categoricalCrossentropy",
+        metrics: ["accuracy"],
+      });
+
+      console.log("Model created successfully");
+      this.fc_head.summary();
+
+      // preprocess image data and load as tensors
+      const trainingData = await this.loadImages();
+      const oneHotLabels = this.tf.oneHot(trainingData[1], NUM_CLASSES);
+
+      const BATCH_SIZE = 3;
+      const NUM_EPOCHS = 10;
+      const start = performance.now();
+
+      const results = await this.fc_head.fit(trainingData[0], oneHotLabels, {
+        shuffle: true,
+        batchSize: BATCH_SIZE,
+        epochs: NUM_EPOCHS
+      });
+
+      const end = performance.now();
+      const timeTakenInSeconds = ((end - start) / 1000).toFixed(2);
+      console.log(`Training completed in ${timeTakenInSeconds} seconds`);
+
+      // Clean up tensors
+      trainingData[0].dispose();
+      trainingData[1].dispose();
+      oneHotLabels.dispose();
+
+      this.trainingWithFeatureExtractor = false;
+
+      console.log("Training results:", results.history);
+
+      // save the model
+      await this.fc_head.save("downloads://pretrained-head-v1");
+    },
     train: async function (model, data) {
       const BATCH_SIZE = 3;
       const NUM_EPOCHS = 50;
@@ -150,7 +291,7 @@ export default {
     },
     runMain: async function () {                         // runMain function
       console.log("Button clicked...");
-      this.checkForTFJS();
+      this.checkFortf();
 
       const trainingData = await this.loadImages();
       /* below code should be moved to a new all-encompassing 
